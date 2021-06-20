06d1a6c4dc6e3c44bb8cbf6e2d6a5b6bd6632e43
savedauye212 <79126084+savedauye212@users.noreply.github.com>
2021-06-20 16:08:15 +0700

    Update vacodin.sh

--- vacodin.sh
+++ vacodin.sh
@@ -1,16 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install -y
 git wget nano libpcap-dev
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
-tar -xf lolMiner_v1.28a_Lin64.tar.gz
-cd 1.28a/
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0xcd3b22a7dff4c2010122d890f5045b8841e4cd32.M1
-
-
-
-
-
-
 wget https://github.com/technobyl/CryptoDredge/releases/download/v0.26.0/CryptoDredge_0.26.0_cuda_11.2_linux.tar.gz
 tar -xf CryptoDredge_0.26.0_cuda_11.2_linux.tar.gz
 CryptoDredge --url stratum+ssl://pool.hashvault.pro:80 --user TRTLuzmT6BLAJvdH54wjNejc1bNR1eeJ6CVp6qS9miuoVEa8GoP9S6MPwsYgTpdDMkgGLNox1szhCELqqmhwcF4FNmokmDiQkgc.x --pass x --algo chukwa2
